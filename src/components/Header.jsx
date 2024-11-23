2e63472e149af2ba61c669d8b1c5494eff77ea1b
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-11-24 02:34:22 +0530

    navbar done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import DesktopNav from "./DesktopNav";
+import Logo from "/logo.svg";
 
 const Header = () => {
   const [hideLeft, setHideLeft] = useState("-left-[1000px]");
@@ -12,7 +14,9 @@ const Header = () => {
   };
   return (
     <>
-      <div className="max-[900px]:hidden">Desktop</div>
+      <div className="max-[900px]:hidden">
+        <DesktopNav menuItems={menuItems} Logo={Logo} />
+      </div>
       <div className="min-[900px]:hidden">Mobile view</div>
     </>
   );
